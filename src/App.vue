45f9cf921211930f24d2e101920af8a419e82c15
ciyoxe <ciyoxe@gmail.com>
2024-08-11 21:46:43 +0700

    Project card

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
 import Breadcrumbs from './components/Breadcrumbs.vue';
+import ProjectCard from './components/ProjectCard.vue';
 </script>
 
 <template>
     <Header />
     <Breadcrumbs :path="['Главная', 'Кейсы']" />
+
+    <ProjectCard
+        description="Тестовое описание товара, пока такое"
+        name="Название кейса"
+        image-url="https://api.test.cyberia.studio/storage/2024/05/01/7a0e7f6cae809a77fb4ef39281ac50e47b399a4f.png"
+    />
 </template>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+    name: string;
+    imageUrl: string;
+    description: string;
+}>();
+</script>
+
+<template>
+    <article
+        class="project-card"
+        :style="{ backgroundImage: `url(${imageUrl})` }"
+    >
+        <div class="project-card__sign">
+            <div class="project-card__sign__name">{{ name }}</div>
+            <img
+                class="project-card__sign__ptr"
+                src="/icons/projectCardPointerBlue.svg"
+            />
+        </div>
+        <section class="project-card__desc-cont">
+            <img
+                class="project-card__ptr"
+                src="/icons/projectCardPointerWhite.svg"
+            />
+            <h3 class="project-card__title">{{ name }}</h3>
+            <p class="project-card__desc">{{ description }}</p>
+        </section>
+    </article>
+</template>
