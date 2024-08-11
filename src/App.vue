da4d0b0f0d7d6515baf744a912c4293cc4fb0102
ciyoxe <ciyoxe@gmail.com>
2024-08-11 18:28:48 +0700

    Breadcrumbs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
+import Breadcrumbs from './components/Breadcrumbs.vue';
 </script>
 
 <template>
     <Header />
+    <Breadcrumbs :path="['Главная', 'Кейсы']" />
 </template>
---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+    path: string[];
+}>();
+</script>
+
+<template>
+    <nav class="breadcrumbs">
+        <template v-for="page in path" :key="page">
+            <a class="breadcrumbs__link">{{ page }}</a>
+            <span class="breadcrumbs__delimiter">/</span>
+        </template>
+    </nav>
+</template>
