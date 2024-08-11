f2d732deef40cb240fedaf809f87178115d329ba
ciyoxe <ciyoxe@gmail.com>
2024-08-11 17:55:56 +0700

    Header + minor improvements

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import SideBar from './components/SideBar.vue';
+import Header from './components/Header.vue';
 </script>
 
 <template>
-    <SideBar />
+    <Header />
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import SideBar from './SideBar.vue';
+
+const menuClosed = ref(true);
+</script>
+
+<template>
+    <header>
+        <img class="header__logo" src="/icons/cyberiaLogo.svg" />
+        <div class="header__cont">
+            <nav class="header__nav">
+                <a class="header__nav__link">Агенство</a>
+                <a class="header__nav__link">Услуги</a>
+                <a class="header__nav__link">Кейсы</a>
+                <a class="header__nav__link">Блог</a>
+                <a class="header__nav__link">Контакты</a>
+            </nav>
+        </div>
+        <button class="header__menu-btn" @click="menuClosed = false" />
+    </header>
+    <SideBar :closed="menuClosed" @close="menuClosed = true" />
+</template>
