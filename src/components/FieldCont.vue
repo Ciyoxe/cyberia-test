2a45f7f21d6805fc0fe361736a686cc12998b5e8
ciyoxe <ciyoxe@gmail.com>
2024-08-16 17:21:32 +0700

    FieldCont now doesn't displaying errors

--- src/components/FieldCont.vue
+++ src/components/FieldCont.vue
@@ -1,34 +1,27 @@
 <script setup lang="ts">
 defineProps<{
     title: string;
-    error?: string;
+    error?: boolean;
 }>();
 </script>
 
 <template>
     <!-- 
         Для "разрезания" рамки поля ввода заголовком пришлось использовать grid.
-        По дизайну поле ввода должно быть прозрачным,
+        По дизайну поле ввода должно быть прозрачным, т.к. на фоне градиент
         закрыть border элементом поверх не получится
     -->
     <div class="field-cont">
-        <div class="field-cont__left" :data-errored="error !== undefined"></div>
-        <div class="field-cont__right" :data-errored="error !== undefined"></div>
-        <div class="field-cont__bottom" :data-errored="error !== undefined"></div>
+        <div class="field-cont__left" :data-errored="error"></div>
+        <div class="field-cont__right" :data-errored="error"></div>
+        <div class="field-cont__bottom" :data-errored="error"></div>
 
         <div class="field-cont__title">
             {{ title }}
-
-            <span class="field-cont__error--desktop" v-if="error">
-                {{ error }}
-            </span>
         </div>
 
         <div class="field-cont__content">
             <slot />
-            <span class="field-cont__error--mobile" v-if="error">
-                {{ error }}
-            </span>
         </div>
     </div>
 </template>
