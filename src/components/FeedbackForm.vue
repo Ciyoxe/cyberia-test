be83ad48000ced5978da10f28426d84b1e75e629
ciyoxe <ciyoxe@gmail.com>
2024-08-16 17:24:34 +0700

    Added agreement and v-model for FeedbackForm

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
 import FieldCont from './FieldCont.vue';
+
+const feedback = reactive({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+    agreement: false,
+});
 </script>
 
 <template>
@@ -8,25 +17,48 @@ import FieldCont from './FieldCont.vue';
 
         <div class="feedback-form__inputs">
             <FieldCont title="Ваше имя*">
-                <input class="feedback-form__input" type="text" placeholder="Ваше имя*" />
+                <input
+                    class="feedback-form__input"
+                    type="text"
+                    placeholder="Ваше имя*"
+                    v-model="feedback.name"
+                />
             </FieldCont>
             <FieldCont title="Email*">
-                <input class="feedback-form__input" type="email" placeholder="Email*" />
+                <input
+                    class="feedback-form__input"
+                    type="email"
+                    placeholder="Email*"
+                    v-model="feedback.email"
+                />
             </FieldCont>
             <FieldCont title="Телефон*">
-                <input class="feedback-form__input" type="tel" placeholder="Телефон*" />
+                <input
+                    class="feedback-form__input"
+                    type="tel"
+                    placeholder="Телефон*"
+                    v-model="feedback.phone"
+                />
             </FieldCont>
         </div>
         <FieldCont title="Сообщение*">
-            <textarea class="feedback-form__textarea" placeholder="Сообщение*"></textarea>
+            <textarea
+                class="feedback-form__textarea"
+                placeholder="Сообщение*"
+                v-model="feedback.message"
+            ></textarea>
         </FieldCont>
 
         <label class="feedback-form__label">
-            <input type="checkbox" class="feedback-form__checkbox" />
+            <input type="checkbox" class="feedback-form__checkbox" v-model="feedback.agreement" />
             Согласие на обработку персональных данных
         </label>
 
-        <button class="feedback-form__btn--desktop">Обсудить проект</button>
-        <button class="feedback-form__btn--mobile">Отправить</button>
+        <button class="feedback-form__btn--desktop" type="button">Обсудить проект</button>
+        <button class="feedback-form__btn--mobile" type="button">Отправить</button>
+
+        <span class="feedback-form__agreement">
+            Нажимая “Отправить”, Вы даете согласие на обработку персональных данных
+        </span>
     </form>
 </template>
