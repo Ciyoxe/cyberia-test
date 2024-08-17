0664d8f867db16f6c8403297470bd233719eaae5
ciyoxe <ciyoxe@gmail.com>
2024-08-17 22:30:20 +0700

    Changed styles to match design

--- src/components/Cases.vue
+++ src/components/Cases.vue
@@ -18,28 +18,32 @@ getCategories({
 </script>
 
 <template>
-    <h2>Кейсы</h2>
-    <div class="cases__categories">
-        <button
-            class="cases__category"
-            v-for="category of projects.categories"
-            :key="category.id"
-            :data-active="projects.filter === category.id"
-            @click="projects.selectCategory(projects.filter === category.id ? null : category.id)"
-        >
-            {{ category.name }}
-        </button>
-    </div>
-    <div class="cases__cards">
-        <ProjectCard
-            v-for="project of projects.filteredProjects"
-            :key="project.id"
-            :name="project.title"
-            :image-url="project.image"
-            :description="project.description"
-        />
-        <template v-if="projects.filteredProjects.length === 0">
-            Упс, кажется, проектов нет
-        </template>
+    <div class="cases">
+        <h2 class="cases__title">Кейсы</h2>
+        <div class="cases__categories">
+            <button
+                class="cases__category"
+                v-for="category of projects.categories"
+                :key="category.id"
+                :data-active="projects.filter === category.id"
+                @click="
+                    projects.selectCategory(projects.filter === category.id ? null : category.id)
+                "
+            >
+                {{ category.name }}
+            </button>
+        </div>
+        <div class="cases__cards">
+            <ProjectCard
+                v-for="project of projects.filteredProjects"
+                :key="project.id"
+                :name="project.title"
+                :image-url="project.image"
+                :description="project.description"
+            />
+            <template v-if="projects.filteredProjects.length === 0">
+                Упс, кажется, проектов нет
+            </template>
+        </div>
     </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,15 +9,13 @@ const menuClosed = ref(true);
     <header>
         <div class="header__content">
             <img class="header__logo" src="/icons/cyberiaLogo.svg" />
-            <div class="header__cont">
-                <nav class="header__nav">
-                    <a class="header__nav__link">Агенство</a>
-                    <a class="header__nav__link">Услуги</a>
-                    <a class="header__nav__link">Кейсы</a>
-                    <a class="header__nav__link">Блог</a>
-                    <a class="header__nav__link">Контакты</a>
-                </nav>
-            </div>
+            <nav class="header__nav">
+                <a class="header__nav__link">Агенство</a>
+                <a class="header__nav__link">Услуги</a>
+                <a class="header__nav__link">Кейсы</a>
+                <a class="header__nav__link">Блог</a>
+                <a class="header__nav__link">Контакты</a>
+            </nav>
             <button class="header__menu-btn" @click="menuClosed = false" />
         </div>
     </header>
